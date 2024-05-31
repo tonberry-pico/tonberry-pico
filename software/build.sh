c8acb86a6bb62f4b77e2e17d4f2d2e8e9f4dd5c3
Matthias Blankertz <matthias@blankertz.org>
2024-05-31 16:52:15 +0200

    Run make with the correct number of jobs

--- software/build.sh
+++ software/build.sh
@@ -5,9 +5,9 @@ TOPDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 set -eu
 
 ( cd lib/micropython
-  make -C mpy-cross -j 16
+  make -C mpy-cross -j $(nproc)
   make -C ports/rp2 BOARD=TONBERRY_RPI_PICO_W BOARD_DIR=$TOPDIR/boards/RPI_PICO_W clean
-  make -C ports/rp2 BOARD=TONBERRY_RPI_PICO_W BOARD_DIR=$TOPDIR/boards/RPI_PICO_W -j 16
+  make -C ports/rp2 BOARD=TONBERRY_RPI_PICO_W BOARD_DIR=$TOPDIR/boards/RPI_PICO_W -j $(nproc)
 )
 
 echo "Output in lib/micropython/ports/rp2/build-TONBERRY_RPI_PICO_W/firmware.uf2"
