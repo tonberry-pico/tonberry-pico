12149986c46518c96299df21f7b61373d7a4bca8
Matthias Blankertz <matthias@blankertz.org>
2024-06-01 13:05:33 +0200

    Add clang-format
    Signed-off-by: Matthias Blankertz <matthias@blankertz.org>

--- software/src/audiocore/audiocore.c
+++ software/src/audiocore/audiocore.c
@@ -6,17 +6,18 @@
 
 #include "py/mperrno.h"
 
-void core1_main(void) {
+void core1_main(void)
+{
     if (!i2s_init(shared_context.out_pin, shared_context.sideset_base, shared_context.samplerate))
-	multicore_fifo_push_blocking(MP_EIO);
+        multicore_fifo_push_blocking(MP_EIO);
 
     multicore_fifo_push_blocking(0);
     uint32_t cmd;
     while ((cmd = multicore_fifo_pop_blocking()) != AUDIOCORE_CMD_SHUTDOWN) {
-	switch (cmd) {
-	default:
-	    break;
-	}
+        switch (cmd) {
+        default:
+            break;
+        }
     }
 
     i2s_deinit();
--- software/src/audiocore/i2s.c
+++ software/src/audiocore/i2s.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2024 Matthias Blankertz <matthias@blankertz.org>
 
-#include "i2s_max98357.pio.h"
 #include "audiocore.h"
+#include "i2s_max98357.pio.h"
 
 #include <hardware/dma.h>
 #include <hardware/sync.h>
@@ -23,20 +23,19 @@ struct i2s_context {
 
 static struct i2s_context i2s_context;
 
-
 static void dma_isr(void)
 {
     if (!dma_channel_get_irq1_status(i2s_context.dma_ch))
-	return;
+        return;
     dma_channel_acknowledge_irq1(i2s_context.dma_ch);
     const uint32_t flags = spin_lock_blocking(shared_context.lock);
     if (audiocore_get_audio_buffer_avail() >= I2S_DMA_BUF_SIZE) {
-	audiocore_audio_buffer_get(i2s_context.dma_buf, I2S_DMA_BUF_SIZE);
-	spin_unlock(shared_context.lock, flags);
+        audiocore_audio_buffer_get(i2s_context.dma_buf, I2S_DMA_BUF_SIZE);
+        spin_unlock(shared_context.lock, flags);
     } else {
-	++shared_context.underruns;
-	spin_unlock(shared_context.lock, flags);
-	memset(i2s_context.dma_buf, 0, sizeof(uint32_t) * I2S_DMA_BUF_SIZE);
+        ++shared_context.underruns;
+        spin_unlock(shared_context.lock, flags);
+        memset(i2s_context.dma_buf, 0, sizeof(uint32_t) * I2S_DMA_BUF_SIZE);
     }
     dma_channel_transfer_from_buffer_now(i2s_context.dma_ch, i2s_context.dma_buf, I2S_DMA_BUF_SIZE);
 }
@@ -51,27 +50,28 @@ bool i2s_init(int out_pin, int sideset_base, int samplerate)
 {
     memset(i2s_context.dma_buf, 0, sizeof(i2s_context.dma_buf[0]) * I2S_DMA_BUF_SIZE);
     if (!pio_can_add_program(audiocore_pio, &i2s_max98357_program))
-	return false;
+        return false;
     i2s_context.pio_sm = pio_claim_unused_sm(audiocore_pio, false);
     if (i2s_context.pio_sm == -1)
-	return false;
+        return false;
     i2s_context.pio_program_offset = pio_add_program(audiocore_pio, &i2s_max98357_program);
-    i2s_max98357_program_init(audiocore_pio, i2s_context.pio_sm, i2s_context.pio_program_offset, out_pin, sideset_base, samplerate);
+    i2s_max98357_program_init(audiocore_pio, i2s_context.pio_sm, i2s_context.pio_program_offset, out_pin, sideset_base,
+                              samplerate);
 
     i2s_context.dma_ch = dma_claim_unused_channel(false);
     if (i2s_context.dma_ch == -1)
-	goto out_dma_claim;
+        goto out_dma_claim;
     setup_dma_config();
     irq_set_exclusive_handler(DMA_IRQ_1, &dma_isr);
-    dma_channel_set_irq1_enabled (i2s_context.dma_ch, true);
+    dma_channel_set_irq1_enabled(i2s_context.dma_ch, true);
     irq_set_enabled(DMA_IRQ_1, true);
     dma_channel_configure(i2s_context.dma_ch, &i2s_context.dma_config, &audiocore_pio->txf[i2s_context.pio_sm],
-			  i2s_context.dma_buf, I2S_DMA_BUF_SIZE, true);
+                          i2s_context.dma_buf, I2S_DMA_BUF_SIZE, true);
     pio_sm_set_enabled(audiocore_pio, i2s_context.pio_sm, true);
-    
+
     return true;
 
- out_dma_claim:
+out_dma_claim:
     pio_remove_program(audiocore_pio, &i2s_max98357_program, i2s_context.pio_program_offset);
     pio_sm_unclaim(audiocore_pio, i2s_context.pio_sm);
     return false;
@@ -80,7 +80,7 @@ bool i2s_init(int out_pin, int sideset_base, int samplerate)
 void i2s_deinit(void)
 {
     pio_sm_set_enabled(audiocore_pio, i2s_context.pio_sm, false);
-    dma_channel_set_irq1_enabled (i2s_context.dma_ch, false);
+    dma_channel_set_irq1_enabled(i2s_context.dma_ch, false);
     dma_channel_unclaim(i2s_context.dma_ch);
     pio_remove_program(audiocore_pio, &i2s_max98357_program, i2s_context.pio_program_offset);
     pio_sm_unclaim(audiocore_pio, i2s_context.pio_sm);
