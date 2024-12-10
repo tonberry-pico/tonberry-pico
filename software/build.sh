1981cac7dee0d1ea24df4b7dc8575fc5a8201ad0
Stefan Kratochwil <Kratochwil-LA@gmx.de>
2024-12-10 21:28:54 +0100

    Check all shell scripts in our repo.

--- software/build.sh
+++ software/build.sh
@@ -5,10 +5,10 @@ TOPDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 set -eu
 
 ( cd lib/micropython
-  make -C mpy-cross -j $(nproc)
-  make -C ports/rp2 BOARD=TONBERRY_RPI_PICO_W BOARD_DIR=$TOPDIR/boards/RPI_PICO_W clean
-  make -C ports/rp2 BOARD=TONBERRY_RPI_PICO_W BOARD_DIR=$TOPDIR/boards/RPI_PICO_W \
-       USER_C_MODULES=$TOPDIR/src/audiocore/micropython.cmake -j $(nproc)
+  make -C mpy-cross -j "$(nproc)"
+  make -C ports/rp2 BOARD=TONBERRY_RPI_PICO_W BOARD_DIR="$TOPDIR"/boards/RPI_PICO_W clean
+  make -C ports/rp2 BOARD=TONBERRY_RPI_PICO_W BOARD_DIR="$TOPDIR"/boards/RPI_PICO_W \
+       USER_C_MODULES="$TOPDIR"/src/audiocore/micropython.cmake -j "$(nproc)"
 )
 
 echo "Output in lib/micropython/ports/rp2/build-TONBERRY_RPI_PICO_W/firmware.uf2"
