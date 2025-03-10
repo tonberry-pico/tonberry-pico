ff1ddfb63909e2a05e8b21752989d3eb61218824
Matthias Blankertz <matthias@blankertz.org>
2025-03-10 20:36:12 +0100

    Add helix_mp3 fixed-point MP3 decoder library

--- software/src/audiocore/micropython.cmake
+++ software/src/audiocore/micropython.cmake
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2024 Matthias Blankertz <matthias@blankertz.org>
 
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../../lib/helix_mp3 helix_mp3)
 add_library(usermod_audiocore INTERFACE)
 
 pico_generate_pio_header(usermod_audiocore ${CMAKE_CURRENT_LIST_DIR}/i2s_max98357.pio)
@@ -16,4 +17,5 @@ target_include_directories(usermod_audiocore INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
+target_link_libraries(usermod_audiocore INTERFACE helix_mp3)
 target_link_libraries(usermod INTERFACE usermod_audiocore)
