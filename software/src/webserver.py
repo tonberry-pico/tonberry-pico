b20a31ccf41762ef164dc9444eb1562438838188
Matthias Blankertz <matthias@blankertz.org>
2025-12-16 21:01:18 +0100

    feat: webserver: redirect / to /index.html
    Signed-off-by: Matthias Blankertz <matthias@blankertz.org>

--- software/src/webserver.py
+++ software/src/webserver.py
@@ -5,7 +5,7 @@ Copyright (c) 2024-2025 Stefan Kratochwil <Kratochwil-LA@gmx.de>
 
 import asyncio
 
-from microdot import Microdot, send_file
+from microdot import Microdot, redirect, send_file
 
 webapp = Microdot()
 server = None
@@ -27,7 +27,7 @@ async def before_request_handler(request):
     app.reset_idle_timeout()
 
 
-@webapp.route('/')
+@webapp.route('/api/v1/hello')
 async def index(request):
     print("wohoo, a guest :)")
     print(f"  app: {request.app}")
@@ -66,12 +66,17 @@ async def config_put(request):
     return '', 204
 
 
+@webapp.route('/', methods=['GET'])
+async def root_get(request):
+    return redirect('/index.html')
+
+
 @webapp.route('/index.html', methods=['GET'])
 async def index_get(request):
     return send_file('/frontend/index.html.gz', content_type='text/html', compressed='gzip')
 
 
-@webapp.route('/static/<path:path>')
+@webapp.route('/static/<path:path>', methods=['GET'])
 async def static(request, path):
     if '..' in path:
         # directory traversal is not allowed
